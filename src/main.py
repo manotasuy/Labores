0a4dcc4e5c1ea6fb0dda299298b9f02bb4a7d8d7
manotasuy <echenique.alvaro@gmail.com>
2020-02-20 21:26:48 -0300

    Algunos cambios para que funcione Heroku

--- src/main.py
+++ src/main.py
@@ -3,20 +3,19 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-@app.route("/<nombre>")
-def hola_mundo(nombre = "invitado"):
-    contexto = {'rotulo' : nombre}
-    return render_template("index.html", **contexto)
+def hola_mundo():
+    return render_template("Login.html")
 
 
-@app.route("/suma/<int:num1>/<int:num2>")
-@app.route("/suma/<int:num1>/<float:num2>")
-@app.route("/suma/<float:num1>/<int:num2>")
-@app.route("/suma/<float:num1>/<float:num2>")
-def suma(num1 = 0, num2 = 0):
-    contexto = {'numero1' : num1, 'numero2' : num2}
-    return render_template("suma.html", **contexto)
+# @app.route("/suma/<int:num1>/<int:num2>")
+# @app.route("/suma/<int:num1>/<float:num2>")
+# @app.route("/suma/<float:num1>/<int:num2>")
+# @app.route("/suma/<float:num1>/<float:num2>")
+# def suma(num1 = 0, num2 = 0):
+#    contexto = {'numero1' : num1, 'numero2' : num2}
+#    return render_template("suma.html", **contexto)
 
 if __name__ == "__main__":
     app.run(debug=True)
