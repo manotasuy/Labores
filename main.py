6f915b295e8704c3238ebbb9e8d2f59a7ab6b359
manotasuy <echenique.alvaro@gmail.com>
2020-02-25 12:18:42 -0300

    Actualizo ip del servidor de MySQL

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask_mysqldb import MySQL
 from Logica import HandlerAdministrador
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'computosMySQLRoot'
 app.config['MYSQL_DB'] = 'bdLabores'
