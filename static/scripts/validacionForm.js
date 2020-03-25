107e1e2e7a3b88a0dd884cc4db534ef759e77409
Danikho2020 <61606567+Danikho2020@users.noreply.github.com>
2020-03-25 20:12:15 -0300

    Arreglado el script para llamar a validacionForm.js Validación de anuncio realizada

--- static/scripts/validacionForm.js
+++ static/scripts/validacionForm.js
@@ -1,26 +1,27 @@
-/* <form ... method="POST" onsubmit="return validarReg();">*/
+
+  /* <form ... method="POST" onsubmit="return validarReg();">*/
 
 /* Validacion para campos de registro de usuario*/
 function validarReg() {
     var nombre, apellido, ci, tel, cumple, domicilio, nacionalidad, email, password, repassword, expresion;
-    
-    nombre = document.getElementById("nombre").value;
-    apellido = document.getElementById("apellido").value;
-    tel = document.getElementById("tel").value;
-    cumple = document.getElementById("cumple").value;
-    domicilio = document.getElementById("domicilio").value;
-    nacionalidad = document.getElementById("nacionalidad").value;
-    email = document.getElementById("email").value;
-    password = document.getElementById("password").value;
-    repassword = document.getElementById("repassword").value;
+
+    nombre = document.forms["perfil"]["nombre"].value;
+    apellido = document.forms["perfil"]["apellido"].value;
+    tel = document.forms["perfil"]["tel"].value;
+    cumple = document.forms["perfil"]["cumple"].value;
+    domicilio = document.forms["perfil"]["domicilio"].value;
+    nacionalidad = document.forms["perfil"]["nacionalidad"].value;
+    email = document.forms["perfil"]["email"].value;
+    password = document.forms["perfil"]["password"].value;
+    repassword = document.forms["perfil"]["repassword"].value;
     
     fecha = new Date();
 
     expresion = / \w+@\w+\.+[a-z] /;
     
   
-    if (nombre.length>30 || nombre.length<2 || !isNaN(nombre)) {
-        alert("Nombre debe tener entre 2 a 30 caracteres alfabeticos");
+    if (nombre.length>30 || nombre.length<3 || !isNaN(nombre)) {
+        alert("Nombre debe tener entre 3 a 30 caracteres alfabeticos");
         return false;
     }
     else if (apellido.length>30 || apellido.length<2 || !isNaN(apellido)) {
@@ -72,14 +73,16 @@ function validarEdEmpleado(){
 function validarEdEmpleador(){
     validarReg();
     validarBps();
+
 }
-/* Validacion para campos de registro Anuncios*/
-function validarAnuncio(){
+/*Validacion para campos de registro Anuncios*/
+
+function validateForm(){
     var titulo, descripcion, pagoPorHora;
 
-    titulo = document.getElementById("titulo").value;
-    descripcion = document.getElementById("descripcion").value;
-    pagoPorHora = document.getElementById("pagoPorHora").value;
+    titulo = document.forms["anuncio"]["titulo"].value;
+    descripcion = document.forms["anuncio"]["descripcion"].value;
+    pagoPorHora = document.forms["anuncio"]["pagoPorHora"].value;
 
     if (titulo.length>30 || titulo.length<4){
         alert("El titulo debe contener de 4 a 30 caracteres");
@@ -89,7 +92,7 @@ function validarAnuncio(){
         alert("La descripcion debe contener de 20 a 350 caracteres")
         return false;
     }
-    else if (isNaN(pagoPorHora) || pagoPorHora.length<2 || pagoPorHora == "00"){
+    else if (isNaN(pagoPorHora) || pagoPorHora.length<2 || pagoPorHora == 0){
         alert("El precio por hora debe de ser correcto");
         return false;
     }
@@ -133,3 +136,29 @@ function validarExt() {
     }
 
 }
+
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function () {
+        'use strict';
+  
+        window.addEventListener('load', function () {
+          // Fetch all the forms we want to apply custom Bootstrap validation styles to
+          var forms = document.getElementsByClassName('needs-validation');
+  
+          // Loop over them and prevent submission
+          var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+              if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+              }
+              form.classList.add('was-validated');
+            }, false);
+          }
+          
+          
+          
+          );
+        }, false);
+      })();
+    
